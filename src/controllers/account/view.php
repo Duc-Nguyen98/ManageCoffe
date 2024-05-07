25c4334d055c4232bf198232a1ede7a0bb0d0ec9
Nguyễn Quang Đức <52225998+Duc-Nguyen98@users.noreply.github.com>
2024-05-07 10:45:07 +0700

    Create view.php

---
+++ src/controllers/account/view.php
@@ -0,0 +1,59 @@
+<?php include '../../layout/header.php'; ?>
+<!--Main layout-->
+
+<main style="margin-top: 58px;">
+    <div class="container-fluid py-4 my-5">
+        <form action="">
+            <div class="row">
+                <div class="col">
+                    <!-- Name input -->
+                    <div data-mdb-input-init class="form-outline">
+                        <input type="text" id="form8Example1" class="form-control" />
+                        <label class="form-label" for="form8Example1">Name</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Email input -->
+                    <div data-mdb-input-init class="form-outline">
+                        <input type="email" id="form8Example2" class="form-control" />
+                        <label class="form-label" for="form8Example2">Email address</label>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+            <div class="row">
+                <div class="col">
+                    <!-- Name input -->
+                    <div data-mdb-input-init class="form-outline">
+                        <input type="text" id="form8Example3" class="form-control" />
+                        <label class="form-label" for="form8Example3">First name</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Name input -->
+                    <div data-mdb-input-init class="form-outline">
+                        <input type="text" id="form8Example4" class="form-control" />
+                        <label class="form-label" for="form8Example4">Last name</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Email input -->
+                    <div data-mdb-input-init class="form-outline">
+                        <input type="email" id="form8Example5" class="form-control" />
+                        <label class="form-label" for="form8Example5">Email address</label>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </form>
+
+    </div>
+</main>
+
+<!--Main layout-->
+
+<?php include '../../layout/footer.php'; ?>
