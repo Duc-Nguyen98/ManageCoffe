c299742b1ea31acfd93c0c13cfd04f6ed9534d39
Nguyễn Quang Đức <52225998+Duc-Nguyen98@users.noreply.github.com>
2024-05-07 10:45:13 +0700

    Update header.php

--- src/layout/header.php
+++ src/layout/header.php
@@ -8,9 +8,9 @@
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
-    <link href="./asset/vendor/googleFonts/css/font.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <!-- MDB -->
-    <link href="./asset/vendor/mdBootstrap/css/mdb.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet" />
 
     <style>
         body {
